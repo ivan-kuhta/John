11f20fbf68a065d528b00e28122fd939c29a1669
Ivan Kuhta <filmplex22342@gmail.com>
2021-03-23 02:17:17 +0200

    feat: add header and menu

--- John/js/script.js
+++ John/js/script.js
@@ -42,17 +42,23 @@ function adaptive_function() {
 }
 
 adaptive_function();;
-$('.header-menu__icon').click(function(event) {
-    $(this).toggleClass('active');
-    $('.header-menu').toggleClass('active');
-    if($(this).hasClass('active')){
-        $('body').data('scroll', $(window).scrollTop());
-    }
-    $('body').toggleClass('lock');
-    if(!$(this).hasClass('active')){
-        $('body, html').scrollTop(parseInt($('body').data('scroll')));
-    }
-});;
+const header = document.querySelector('header');
+header.querySelector('.menu__icon').addEventListener('click', () => {
+    header.querySelector('.menu__icon').classList.toggle('active');
+    header.querySelector('.menu__body').classList.toggle('active');
+}) 
+
+// $('.menu__icon').click(function(event) {
+//     console.log($(this).toggleClass('active'));
+//     $('.menu__icon').toggleClass('active');
+//     // if($(this).hasClass('active')){
+//     //     $('body').data('scroll', $(window).scrollTop());
+//     // }
+//     // $('body').toggleClass('lock');
+//     // if(!$(this).hasClass('active')){
+//     //     $('body, html').scrollTop(parseInt($('body').data('scroll')));
+//     // }
+// });;
 function ibg(){
     $.each($('.ibg'), function(index, val) {
         if($(this).find('img').length>0){
